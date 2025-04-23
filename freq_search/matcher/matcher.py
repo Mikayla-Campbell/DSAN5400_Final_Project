5fbee95b014d716dc80563a9bf585ae15f63b278
Mikayla-Campbell <mrc195@georgetown.edu>
2025-04-23 01:29:52 -0400

    Update matcher.py

---
+++ freq_search/matcher/matcher.py
@@ -0,0 +1,43 @@
+class matcher:
+    def __init__(self, ):
+
+class realmatcher(matcher):
+    def 
+
+dict_list = {
+    "ability":[
+        r"accessib(le|ility)",
+        r"(dis)?ability",
+        "mental health",
+        "people-centered care",
+        "at risk"
+    ],
+    "class":[
+        r"oppress(ion|ive)",
+        r"privileges?",
+        "socioeconomic",
+        "status",
+        r"systemic(ally)?"
+    ],
+    "climate":[
+        "clean energy",
+        r"climate (crisis|science)",
+        r"environment(al)?",
+        "pollution"
+    ],
+    "gs":[
+        r"gender ?(ideology|diversity|based|-affirming)",
+        r"assigned (fe)?male at birth",
+        r"sexual(ity)? (preferences)?",
+        r"LGBTQ?",
+        r"trans(gender|sexual)"
+    ],
+    "re":[
+        r"(multi)?cultural(ly)? ?(competence|differences|heritage|sensitivity|appropriate|responsive)?",
+        r"ethnic(ity)?",
+        r"rac(e|ial(ly)?|ism)",
+        "Latinx",
+        "Native American",
+        "Black",
+    ]
+}
