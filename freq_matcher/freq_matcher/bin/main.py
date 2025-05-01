146f6e1c9eb418f22fa7700bc4b6be6c0e28479b
BrookeBergin <brookebergin27@gmail.com>
2025-04-30 20:26:29 -0400

    Add files via upload

--- freq_matcher/freq_matcher/bin/main.py
+++ freq_matcher/freq_matcher/bin/main.py
@@ -2,6 +2,7 @@ import argparse
 from pprint import pprint
 import logging
 from freq_matcher.freq_matcher import Matcher
+from utility.Process_data import download_data
 
 def process_tsv(filepath):
     abstract_list = []
@@ -12,10 +13,28 @@ def process_tsv(filepath):
 
 if __name__ == '__main__':
     # configure logging
+    for handler in logging.root.handlers[:]:
+        logging.root.removeHandler(handler)
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                         filename='../../logs.txt')
+    logging.getLogger().addHandler(logging.StreamHandler())
+    logging.info("Entered main.")
 
     # import files
+    october_url = 'https://drive.google.com/uc?export=download&id=15O5--LaJl3S3nIJ00EpDCr0oNh8UiZDi'
+    november_url = 'https://drive.google.com/uc?export=download&id=1PIsMTxY5zmTEjU73dII07XGdCG7J2FhE'
+    december_url = ' https://drive.google.com/uc?export=download&id=1H2mPOoQvOdHgb1176ieZHAoUrhIVYa-5'
+    january_url = 'https://drive.google.com/uc?export=download&id=1ZrwmQ8KwebNkni-fnguRiUfKq_n0tce_'
+    february_url = ' https://drive.google.com/uc?export=download&id=1IXGPA0K_A038DAeIFCvTy2eJNH599XOj'
+    march_url = 'https://drive.google.com/uc?export=download&id=1i0VVWp4lSswgNb3227LvDOtFEHaKDZ-s'
+
+    download_data(october_url, '../../data/2410oct.tsv')
+    download_data(november_url, '../../data/2411nov.tsv')
+    download_data(december_url, '../../data/2412dec.tsv')
+    download_data(january_url, '../../data/2501jan.tsv')
+    download_data(february_url, '../../data/2502feb.tsv')
+    download_data(march_url, '../../data/2503mar.tsv')
+
     october = '../../data/2410oct.tsv'
     november = '../../data/2411nov.tsv'
     december = '../../data/2412dec.tsv'
@@ -29,7 +48,6 @@ if __name__ == '__main__':
     abstracts_jan = process_tsv(january)
     abstracts_feb = process_tsv(february)
     abstracts_mar = process_tsv(march)
-
 
     # filter user request by argparse
     parser = argparse.ArgumentParser()
